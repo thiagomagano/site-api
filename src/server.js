282c2ee82f5b6150ffa3ce5c14d2d8cfdfccf8ba
Thiago Magano <thiagomagano1993@outlook.com>
2025-11-26 15:33:52 -0300

    bootstrap: add middlaware helmet and cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
