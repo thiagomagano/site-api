705f0a3c5d51270871e82ba4b5404169a71e7c4a
Thiago Magano <thiagomagano1993@outlook.com>
2025-11-26 15:28:02 -0300

    bootstrap: add dotenv, json middleware, port envs

--- src/server.js
+++ src/server.js
@@ -1,19 +1,23 @@
-import express, { json } from "express";
-const app = express();
-const port = 3000;
+import express from "express";
+import dotenv from "dotenv";
 
-app.use(json());
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ status: 200, msg: "Hello, from Thiagomagano.com.br REST API" });
+  res.json({
+    message: "Hello from thiagomagano.com.br API",
+    enviroment: process.env.NODE_ENV,
+  });
 });
 
 app.get("/api/health", (req, res) => {
-  res.json({ status: 200, msg: "API SAUDAVEL!" });
+  res.json({ message: "API SAUDÁVEL!" });
 });
 
 app.listen(port, () => {
-  console.log(
-    `thiagomagano.com.br API is running on port http://localhost:${port}`,
-  );
+  console.log(`thiagomagano.com.br API is running on http://localhost:${port}`);
 });
