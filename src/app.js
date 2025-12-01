7bd6a514b68948844ee870d419442e52e6c21e3f
Thiago Magano <thiagomagano1993@outlook.com>
2025-12-01 20:22:54 -0300

    adjust cors policy

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || "http://localhost:3000",
+  origin: process.env.CORS_ORIGIN || "http://localhost:5173",
   credentials: true
 }));
 app.use(express.json());
