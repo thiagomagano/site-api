d3dfd423b7053f4500da3e62f94076921e72fbce
Thiago Magano <thiagomagano1993@outlook.com>
2025-12-01 15:40:12 -0300

    test: add GET /projects

--- tests/projects.test.js
+++ tests/projects.test.js
@@ -2,6 +2,7 @@ import { describe, it, expect, beforeAll, afterAll, beforeEach } from "vitest";
 import request from "supertest";
 import app from "../src/app.js";
 import { connectMemoryDB, disconnectMemoryDB, clearCollections } from "./mongo-memory.js";
+import Project from "../src/models/Project.js";
 
 
 
@@ -18,10 +19,37 @@ describe("Rotas de /projects (com mongo em memória)", () => {
     await disconnectMemoryDB();
   });
 
-  it("GET /api/projects deve responder com status 200 e array", async () => {
-    const res = await request(app).get("/api/projects");
+  describe("GET /api/projects", async () => {
 
-    expect(res.status).toBe(200);
-    expect(Array.isArray(res.body)).toBe(true);
+    it("deve responder com status 200 e array bazio quando não há projetos", async () => {
+      const res = await request(app).get("/api/projects");
+
+      expect(res.status).toBe(200);
+      expect(Array.isArray(res.body)).toBe(true);
+      expect(res.body).toHaveLength(0);
+    })
+
+    it("deve retornar todos os projetos quando existem", async () => {
+      await Project.create({
+        title: "Projeto 1",
+        description: "Descrição do projeto 1",
+        stack: ["nodedotjs", "express"],
+      });
+
+      await Project.create({
+        title: "Projeto 2",
+        description: "Descrição do projeto 2",
+        stack: ["react", "svelte"],
+      });
+
+
+      const res = await request(app).get("/api/projects");
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveLength(2);
+      expect(res.body[0]).toHaveProperty("_id");
+      expect(res.body[0]).toHaveProperty("title");
+    })
+
   })
 });
